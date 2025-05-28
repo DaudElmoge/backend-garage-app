b81354dae69975dd1524fd3b37c9252c1b186f53
Daud Abdiwahab <daudwahab1320@gmail.com>
2025-05-28 21:19:39 +0300

    defined customer routes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import session
 from models import Base, engine, get_db, Customer, ServiceRecord, RepairRecord, CarMake, Mechanic
-import schemas
+from schemas import CustomerSchema
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,7 +11,7 @@ app = FastAPI()
 #Frontend requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000"],
+    allow_origins=["http://localhost:5173/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -22,3 +22,32 @@ app.add_middleware(
 def index():
     return {"message":"Welcome to AWAG garage management"}
 
+#customers
+@app.post("/customers")
+def add_customer(customer: CustomerSchema, db: session = Depends(get_db)):
+    new_customer = Customer(**customer.dict())
+    db.add(new_customer)
+    db.commit()
+    return {"message":"Success"}
+
+@app.get("/customers")
+def get_customers(db:session=Depends(get_db)):
+    return db.query(Customer).all()
+
+@app.get("/customers/search/")
+def search_customer(phone:str,db:session=Depends(get_db)):
+    customer = db.query(Customer).filter(Customer.phone==phone).first()
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return customer
+    
+@app.delete("/customers/{customer_id}")
+def delete_customer(customer_id: int, db: session=Depends(get_db)):
+    customer = db.query(Customer).filter(Customer.id==customer_id).first()
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    db.delete(customer)
+    db.commit()
+    return {"message":"Customer deleted successfully"}
+
+#Service Records
